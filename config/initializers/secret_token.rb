7f0bee25c08a495b7171226dc0762f12a6a46ca2
jesusblessf6 <jesusblessf6@gmail.com>
2013-04-30 19:07:38 +0800

    init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankProduct::Application.config.secret_token = '400a51de7657c19d210f16237c572dbbd46dab2fa48ddff3f6c3542c6c70f5d372fe220fa9b00b8a62a8599031e080dd8d6211dfe8700d489ecffa8b281a3ff8'
