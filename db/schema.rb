87e17853b06ccd144c4ac2d10f890adf4e0b337e
wing <jesusblessf6@gmail.com>
2013-05-14 11:48:52 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130509144427) do
+ActiveRecord::Schema.define(:version => 20130514034639) do
 
   create_table "banks", :force => true do |t|
     t.string   "name"
@@ -71,6 +71,14 @@ ActiveRecord::Schema.define(:version => 20130509144427) do
     t.integer  "shop_category_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
