32c56df11fc55dc74e161871b3cc1a2a39253cd2
jesusblessf6 <jesusblessf6@gmail.com>
2013-05-23 23:53:23 +0800

    update

--- app/controllers/sign_ins_controller.rb
+++ app/controllers/sign_ins_controller.rb
@@ -4,8 +4,15 @@ class SignInsController < ApplicationController
   end
 
   def create
-  	flash.now[:message] = "hello"
-  	render 'new'
+  	user = User.find_by_email(params[:sign_in][:email].downcase)
+  	if user && user.authenticate(params[:sign_in][:password])
+  		session[:current_user_id] = user.id
+  		current_user = user
+  		redirect_to home_path
+  	else
+  		flash.now[:error] = "email or password error!"
+  		render 'new'
+  	end
   end
 
   def destroy
